23ca54d29e5c2e69a3ecce34919bdc1443eb16e6
GURUZ ANTONY MERSON S <guruzantony000@gmail.com>
2025-03-18 17:17:09 +0530

    Add files via upload
    A simple C code to find factorial using functions

---
+++ factorial.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+// Factorial of the given number
+int Fact(int number){
+    if(number==0 || number==1){
+        return 1;
+    }
+    else{
+        return number*Fact(number-1);
+    }
+}
+
+
+
+
+int main() {
+    int number;
+    printf("Enter the number to calculate the factorial:");
+    scanf("%d",&number);
+    printf("The factorial of %d is : %d",number, Fact(number));
+  
+}
